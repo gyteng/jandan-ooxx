ed82000c0e99bc810352fe826c4b3420a87b50dd
Guyuteng <igyteng@gmail.com>
2016-12-07 12:43:38 +0800

    null

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -84,7 +84,7 @@ app
       $scope.getImages();
       $scope.random = () => {
         if($scope.images.length < 2) {
-          $scope.getImages();
+          return $scope.getImages();
         }
         $scope.images.splice(0, 1);
         $localStorage.imagesHistory.push($scope.images[0]);
