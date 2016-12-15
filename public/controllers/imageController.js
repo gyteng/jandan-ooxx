7d33cc0bdcfe31063ea172324b84afbec699e427
Guyuteng <igyteng@gmail.com>
2016-12-15 15:07:59 +0800

    manager button

--- public/controllers/imageController.js
+++ public/controllers/imageController.js
@@ -1,12 +1,14 @@
 const app = require('../index').app;
 
 app
-  .controller('ImageController', ['$scope', '$stateParams', '$http',
-    ($scope, $stateParams, $http) => {
+  .controller('ImageController', ['$scope', '$stateParams', '$http', '$interval',
+    ($scope, $stateParams, $http, $interval) => {
       const id = $stateParams.id;
       $scope.setCurrentImage(id);
       $scope.public.isFavorite = false;
       $scope.public.view.push(id);
+
+      $scope.isManagerMenuOpen = false;
     }
   ])
 ;
