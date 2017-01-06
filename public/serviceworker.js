5034af7e253696ac34358a3d2606ec0f0bdc5f09
gyt-Macmini <igyteng@gmail.com>
2017-01-06 21:23:10 +0800

    address bar color

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,6 +1,6 @@
 importScripts('/libs/serviceworker-cache-polyfill.js');
 
-var ONLINE_CACHE_NAME = 'OOXX 2017-01-04 18:03';
+var ONLINE_CACHE_NAME = 'OOXX 2017-01-06 21:20';
 var onlineCacheUrl = [
   '/',
 
